8e06bfff0af44c6060e3266ac761fec419ac3a7c
gooooloo <qidu.lin@gmail.com>
2013-07-17 22:10:54 +0800

    apply GPL license

--- src/com/qidu/lin/nonetworkplease/MainActivity.java
+++ src/com/qidu/lin/nonetworkplease/MainActivity.java
@@ -1,6 +1,22 @@
-/**
- * Author : gooooloo (https://github.com/gooooloo)
+/*
+ * Copyright 2013 Qidu Lin
+ * 
+ * This file is part of NoNetworkPlease.
+ * 
+ * NoNetworkPlease is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * NoNetworkPlease is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * NoNetworkPlease. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.qidu.lin.nonetworkplease;
 
 import java.lang.reflect.Field;
